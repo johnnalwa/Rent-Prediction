62be08fe9960d89aa81d0a0e4b13910ee3f49dce
mary <marynyamburah321@gmail.com>
2024-03-28 16:00:09 +0300

    new

--- RentPrediction/accounts/forms.py
+++ RentPrediction/accounts/forms.py
@@ -11,3 +11,9 @@ class userForm(forms.ModelForm):
 class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput)
+    
+    
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput)
